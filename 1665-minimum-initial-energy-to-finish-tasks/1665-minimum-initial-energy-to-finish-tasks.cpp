7b9f93e94d38df93dcf4085f80810c6a8d1bcdad
Adwait Mishra <mishraadwait456@gmail.com>
2026-05-12 18:25:18 +0530

    Time: 39 ms (60.68%), Space: 97.4 MB (39.81%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int minimumEffort(vector<vector<int>>& task) {
-        sort(task.begin(), task.end(), [](vector<int>& a, vector<int>& b) {
+        sort(task.begin(), task.end(), [](const vector<int>& a, const vector<int>& b) {
             int gap1 = a[1] - a[0];
             int gap2 = b[1] - b[0];
             return gap1 < gap2;
         });
         int ans = 0;
         for(int i = 0; i < task.size(); i++) {
-            if(ans + task[i][0] >= task[i][1]) ans += task[i][0];
-            else ans = task[i][1];
+            ans = max(ans + task[i][0], task[i][1]);
         }
         return ans;
     }
