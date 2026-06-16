e118ce41203ac341188dded653f9fd627bb22c9e
Adwait Mishra <mishraadwait456@gmail.com>
2026-06-16 17:07:30 +0530

    Time: 8 ms (26.89%), Space: 39.7 MB (69.7%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string ans = "";
+        for(char c : s) {
+            if(c != '*' && c != '#' && c != '%') ans += c;
+            else if(c == '*' && ans.size() > 0) ans.pop_back();
+            else if(c == '#') ans += ans;
+            else if(c == '%') reverse(ans.begin(), ans.end());
+        }
+        return ans;
+    }
+};
