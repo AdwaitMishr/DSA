0581b0532b9271843b87605308975c63dc6a46d2
Adwait Mishra <mishraadwait456@gmail.com>
2026-07-30 18:34:41 +0530

    Time: 0 ms (100%), Space: 8.3 MB (77.73%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int ans = 0;
-        for(int i = 0; i < word.size(); i++) ans += i / 8 + 1;
-        return ans;
+        int n = word.size();
+        int key = n >> 3;
+        return (key * (key + 1) * 4) + (n % 8) * (key + 1);
     }
 };
